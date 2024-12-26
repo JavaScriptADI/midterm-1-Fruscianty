2e948209a6a7493128e8ea5a3b1b78af6ed3da90
Fruscianty <anri.2002.xndr@gmail.com>
2024-12-27 01:08:22 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-// Write your code here
+//1st 
+let currentDay = "Tuesday";
+let currentTime = "10 PM : 30 : 38";
+
+console.log(`Today is: ${currentDay}. Current time is: ${currentTime} `);
+
+//2nd
+window.print();
+
+//3rd
+let month = 12;
+let day = 27;
+let year = 2024;
+
+console.log(`${month}/${day}/${year}`);
+
+//4th
